7b1c0b65e61b03356d0d4a4bd5c51b5c70592099
anselm <anselm@gmail.com>
2024-10-02 00:16:08 -0700

    corrected esbuild esm module import.meta

--- esbuild-make.mjs
+++ esbuild-make.mjs
@@ -3,11 +3,15 @@ import * as esbuild from 'esbuild'
 await esbuild.build({
 	entryPoints: ['src/main.ts'],
 	bundle: true,
+	format: 'esm',
 	outfile: 'dist/main.js',
+	target: 'chrome64',
 })
 
 await esbuild.build({
 	entryPoints: ['src/puppet/llm-worker.js'],
 	bundle: true,
+	format: 'esm',
 	outfile: 'dist/llm-worker.js',
+	target: 'chrome64',
 })
